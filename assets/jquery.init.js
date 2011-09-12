5fe5eda10e2fdece2d8000da18bbce98db11ef87
metamn <bartus.csongor@gmail.com>
2011-09-12 20:23:40 +0300

    Triangle colors are working fine now

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -16,6 +16,8 @@ jQuery(document).ready(function(){
   // Change colors on menu navigation
   jQuery("#collections a").hover (    
     function () {      
+      removeColors();
+      
       var color = jQuery(this).attr('class');
       var klass = color + "-triangle";
       jQuery("#divider .triangle-top").addClass(klass);
@@ -24,7 +26,17 @@ jQuery(document).ready(function(){
       var color = jQuery(this).attr('class');
       var klass = color + "-triangle";
       jQuery("#divider .triangle-top").removeClass(klass);
+      // Restore original klass, if any
+      jQuery("#divider .triangle-top").addClass(jQuery("#divider .triangle-top").attr("id"));
     }
   );
+  
+  // Remove all color classes which might be set previously
+  function removeColors() {
+    var a = ["bestseller", "latest", "sale", "featured"]
+    for (var i = 0; i < a.length; i++) {
+      jQuery("#divider .triangle-top").removeClass(a[i]+"-triangle");
+    }
+  }
 
 });
