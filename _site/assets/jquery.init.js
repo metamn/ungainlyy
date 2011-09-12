813d42654fee3a88f2ec7dcdf6ff6480f5d3e20c
metamn <bartus.csongor@gmail.com>
2011-09-12 19:44:55 +0300

    Scrol themes added

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -3,6 +3,17 @@ jQuery.noConflict();
 // Use jQuery via jQuery(...)
 jQuery(document).ready(function(){
 
+  // Show the Soros book
+  jQuery(".soros").hover (
+    function() {
+      jQuery("#soros").show('slow');
+    },
+    function () {
+    
+    }  
+  );
+
+  // Change colors on menu navigation
   jQuery("#collections a").hover (    
     function () {      
       var color = jQuery(this).attr('class');
